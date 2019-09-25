388cf730179e1cf0f6a84552245dfd26dc45fba9
Guillermo Andrés García Vázquez <a01283254@itesm.mx>
2019-09-25 14:46:51 -0500

    Commented name at the top

--- DataStructures_Postfix/DataStructures_Postfix.cpp
+++ DataStructures_Postfix/DataStructures_Postfix.cpp
@@ -1,9 +1,113 @@
-// DataStructures_Postfix.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//Guillermo García
 #include <iostream>
+using namespace std;
+#include <stack>
+#include <queue>
+#include <functional>
+#include <sstream>
 
 int main()
 {
-    
+	stack<int> miStack;
+	queue<int> miQueue;
+	priority_queue<int, vector<int>, greater<int>> miPQ; //Con MIN-HEAP
+
+	string data, d;
+	getline(cin, data);
+
+	int x, y, res;
+
+	while (data != "#") {
+		stringstream ss;
+		ss << data;
+		while (ss >> d) {
+			//PROCESOS
+			if (d == "+") {
+				//STACK
+				x = miStack.top();
+				miStack.pop();
+				y = miStack.top();
+				miStack.pop();
+
+				miStack.push(y + x);
+
+				//QUEUE
+				x = miQueue.front();
+				miQueue.pop();
+				y = miQueue.front();
+				miQueue.pop();
+
+				miQueue.push(y + x);
+
+				//PRIORITY QUEUE
+				x = miPQ.top();
+				miPQ.pop();
+				y = miPQ.top();
+				miPQ.pop();
+
+				miPQ.push(y + x);
+			}
+			else if (d == "-") {
+				//STACK
+				x = miStack.top();
+				miStack.pop();
+				y = miStack.top();
+				miStack.pop();
+
+				miStack.push(y - x);
+
+				//QUEUE
+				x = miQueue.front();
+				miQueue.pop();
+				y = miQueue.front();
+				miQueue.pop();
+
+				miQueue.push(y - x);
+
+				//PRIORITY QUEUE
+				x = miPQ.top();
+				miPQ.pop();
+				y = miPQ.top();
+				miPQ.pop();
+
+				miPQ.push(y - x);
+			}
+			else if (d == "*") {
+				//STACK
+				x = miStack.top();
+				miStack.pop();
+				y = miStack.top();
+				miStack.pop();
+
+				miStack.push(y * x);
+
+				//QUEUE
+				x = miQueue.front();
+				miQueue.pop();
+				y = miQueue.front();
+				miQueue.pop();
+
+				miQueue.push(y * x);
+
+				//PRIORITY QUEUE
+				x = miPQ.top();
+				miPQ.pop();
+				y = miPQ.top();
+				miPQ.pop();
+
+				miPQ.push(y * x);
+			}
+			else {
+				miStack.push(stoi(d));
+				miQueue.push(stoi(d));
+				miPQ.push(stoi(d));
+			}
+		}
+		cout << miStack.top() << " " << miQueue.front() << " " << miPQ.top() << endl;
+		miStack.pop();
+		miQueue.pop();
+		miPQ.pop();
+
+		getline(cin, data);
+	}
 }
